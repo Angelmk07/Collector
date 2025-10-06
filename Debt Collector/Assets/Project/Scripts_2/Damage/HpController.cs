ce6ab0cd42982d78130b3405a90c4fdc8578949d
BadCoder264 <greengamedeveloper51@gmail.com>
2025-10-07 01:16:46 +0300

    NewFix

--- Debt Collector/Assets/Project/Scripts_2/Damage/HpController.cs
+++ Debt Collector/Assets/Project/Scripts_2/Damage/HpController.cs
@@ -6,12 +6,14 @@ using UnityEngine.Events;
 public class HpController : MonoBehaviour
 {
     [SerializeField] private int hp;
+    [SerializeField] private GameObject coin;
     public UnityEvent OnDead;
 
     public void TakeDamage(int value)
     {
         hp -= value;
         OnDead.Invoke();
+        Instantiate(coin, transform.position, Quaternion.identity);
     }
 
 }
